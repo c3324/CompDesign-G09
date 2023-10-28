4df311fbb49a8546033a03bda2df2912362a7520
c3324 <c3324819@uon.edu.au>
2023-10-28 13:05:50 +1100

    merge fix

---
+++ COMP3290 Assignments/src/Codegen_Evie.java
@@ -0,0 +1,16 @@
+//need to feed syntaxTree into this class to do the code generation. 
+
+import java.util.*;
+
+public class Codegen_Evie {
+
+    private LinkedList<String> instructions;
+	private String opcodeStr;
+
+    public Codegen_Evie() {
+		this.instructions = new LinkedList<String>();
+		this.opcodeStr = "";
+	}
+
+    
+}
